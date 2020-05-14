7591713941766d3c4622cecf124c7859b4a97238
Jake Dahl <jakedahl16@yahoo.com>
2020-05-14 15:01:21 -0500

    notes2

--- client.js
+++ client.js
@@ -1,17 +1,24 @@
 console.log('hi');
-$(document).ready(onReady)
+$(document).ready(onReady);
 
 function onReady() {
     console.log('jQuery is loaded');
     $('#Hello').append('Welcome to my Page!');
     //$('#Hello').css('color', 'red');   Not the best way to do things
     $('#Hello').addClass('blue'); //best practice
-    $('#submit').on('click', handleSubmit;
+    $('#submit').on('click', handleSubmit);
+    $('.extraBtn').on('click', '.extraBtn', handleClick);
+  
 }//testing to see if jQuery works
 
-    funciton handleSubmit(){
+    function handleSubmit() {   
     console.log('hello');
     let firstName = $('#firstName').val();
-    console.log('');
-    $('#firstName').val('yay')
+    console.log(firstName);
+    $('#firstName').val('yay');
+    $('#wrapper').append('<button class="extraBtn>Click Me</button>');
 }
+ function handleClick() {
+     console.log('button clicked');
+
+ }
