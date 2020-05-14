8be5a439b5d33412e03917d100e16f821bfdf100
Jake Dahl <jakedahl16@yahoo.com>
2020-05-14 14:36:00 -0500

    notes

--- client.js
+++ client.js
@@ -1,1 +1,17 @@
 console.log('hi');
+$(document).ready(onReady)
+
+function onReady() {
+    console.log('jQuery is loaded');
+    $('#Hello').append('Welcome to my Page!');
+    //$('#Hello').css('color', 'red');   Not the best way to do things
+    $('#Hello').addClass('blue'); //best practice
+    $('#submit').on('click', handleSubmit;
+}//testing to see if jQuery works
+
+    funciton handleSubmit(){
+    console.log('hello');
+    let firstName = $('#firstName').val();
+    console.log('');
+    $('#firstName').val('yay')
+}
